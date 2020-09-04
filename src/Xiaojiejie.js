de868003d0ea56df8dac255c10161281d02f1163
Rongzhi Wang <aidenwang0826@gmail.com>
2020-09-04 20:42:30 +1000

    add react-transition-group and axios

--- src/Xiaojiejie.js
+++ src/Xiaojiejie.js
@@ -1,18 +1,51 @@
-import React, {Component, Fragment} from 'react'
+import React, { Component, Fragment } from 'react'
+import axios from 'axios'
 import './style.css'
 import XiaojiejieItem from './XiaojiejieItem'
+import Boss from './Boss'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
-class Xiaojiejie extends Component{
-    constructor(props){
+class Xiaojiejie extends Component {
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             inputValue: 'new service',
-            list:['junior','middle']
+            list: ['junior', 'middle']
         }
     }
 
-    render(){
-        return(
+    componentWillMount() {
+        console.log('componentWillMount ------ start of lifecycle')
+    }
+
+    /**componentDidMount(){
+        //console.log('componentDidMount ------ end of lifecycle')
+        axios.get('https://web-api.juejin.im/v3/web/wbbr/bgeda')
+            .then((res)=>{
+                console.log('axios success'+JSON.stringify(res))
+                this.setState({
+                    list:res.data.data
+                })
+            })
+            .catch((error)=>{console.log('axios fail:'+error)})
+    }*/
+
+    shouldComponentUpdate() {
+        //console.log('1-shouldComponentUpdate')
+        return true
+    }
+
+    componentWillUpdate() {
+        //console.log('2-componentWillUpdate')       
+    }
+
+    componentDidUpdate() {
+        //console.log('4-componentDidUpdate')       
+    }
+
+    render() {
+        //console.log('3-render------ middle of lifecycle')
+        return (
             // flex
             <Fragment>
                 {/* comment line */}
@@ -21,16 +54,36 @@ class Xiaojiejie extends Component{
                 }
                 <div>
                     <label htmlFor="formlabel">add servise:</label>
-                    <input id="formlabel" className="input" value={this.state.inputValue} onChange={this.inputChange.bind(this)} />
+                    <input
+                        id="formlabel"
+                        className="input"
+                        value={this.state.inputValue}
+                        onChange={this.inputChange.bind(this)}
+                        ref={(input) => { this.input = input }}
+                    />
                     <button onClick={this.addList.bind(this)}>add service</button>
                 </div>
-                    <ul>
+                <ul ref={(ul) => { this.ul = ul }}>
+                    <TransitionGroup>
                         {
-                            this.state.list.map((item, index)=>{
-                                return( 
-                                        <XiaojiejieItem key={index+item} content={item} index={index}
+                            this.state.list.map((item, index) => {
+                                return (
+                                    <CSSTransition
+                                        timeout={2000}
+                                        classNames='boss-text'
+                                        unmountOnExit
+                                        appear={true}
+                                        key={index + item}
+                                        //key={item}
+                                    >
+                                        <XiaojiejieItem
+                                            //key={index + item}
+                                            content={item}
+                                            index={index}
                                             deleteItem={this.deleteItem.bind(this)}
+                                        //name="people"
                                         />
+                                    </CSSTransition>
                                     /** 
                                         <li 
                                         key={index+item} 
@@ -38,38 +91,45 @@ class Xiaojiejie extends Component{
                                         dangerouslySetInnerHTML={{__html:item}}
                                         >
                                         </li>
-                                     */     
+                                     */
                                 )
                             })
                         }
-                    </ul>
-               
+                    </TransitionGroup>
+                </ul>
+
+                <Boss />
+
             </Fragment>
         )
     }
 
-    inputChange(e){
+    inputChange(e) {
         // console.log(this)
         // this.state.inputValue = e.target.value
         this.setState({
-            inputValue: e.target.value
+            inputValue: this.input.value
         })
     }
 
-    addList(){
+    addList() {
         this.setState({
-            list:[...this.state.list, this.state.inputValue]
+            list: [...this.state.list, this.state.inputValue]
+        }, () => {
+            console.log(this.ul.querySelectorAll('li').length)
         })
+
     }
 
-    deleteItem(index){
+    deleteItem(index) {
         console.log(index)
         let list = this.state.list
         list.splice(index, 1)
         this.setState({
-            list:list
+            list: list
         })
     }
 }
 
 export default Xiaojiejie
+
