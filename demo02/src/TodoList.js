453fd02cacc95c7600f6098fa5bc788637a1271b
Rongzhi Wang <aidenwang0826@gmail.com>
2020-09-08 08:49:41 +1000

    add react-redux

--- demo02/src/TodoList.js
+++ demo02/src/TodoList.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
-class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
+//import store from './store'
+import { connect } from 'react-redux'
+
+const TodoList = (props) => {
+    let { inputValue, inputChange, clickButton, list } = props
+    return (
+        <div>
             <div>
-                Hello World
+                <input
+                    value={inputValue}
+                    onChange={inputChange}
+                />
+                <button
+                    onClick={clickButton}
+                >add</button>
             </div>
-         );
+            <ul>
+                {
+                    list.map((item, index) => {
+                        return (<li key={index}>{item}</li>)
+                    })
+                }
+            </ul>
+        </div>
+    );
+}
+
+const dispatchToProps = (dispatch) => {
+    return {
+        inputChange(e) {
+            let action = {
+                type: 'change_input',
+                value: e.target.value
+            }
+
+            dispatch(action)
+        },
+
+        clickButton() {
+            let action = {
+                type: 'add_item'
+            }
+
+            dispatch(action)
+        }
     }
 }
- 
-export default TodoList;
+
+const stateToProps = (state) => {
+    return {
+        inputValue: state.inputValue,
+        list: state.list
+    }
+}
+
+export default connect(stateToProps, dispatchToProps)(TodoList);
