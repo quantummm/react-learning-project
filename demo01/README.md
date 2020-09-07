739b5ecbdf6f52f1f6e87999a65b86d35de1992e
Rongzhi Wang <aidenwang0826@gmail.com>
2020-09-07 19:51:56 +1000

    initial commit

---
+++ demo01/README.md
@@ -0,0 +1,6 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Redux-TodoList
+
+Create a basic TodoList by using redux
+
---
+++ demo02/src/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                Hello World
+            </div>
+         );
+    }
+}
+ 
+export default TodoList;
